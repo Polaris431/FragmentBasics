0d1897718e34ef27515ba1bcccc19fb59ea18a1c
Johann Blake <polaris431@gmail.com>
2014-06-20 13:18:28 +0200

    Added comments

--- app/src/main/java/com/example/android/fragments/ArticleFragment.java
+++ app/src/main/java/com/example/android/fragments/ArticleFragment.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * The fragment used for articles.
  */
 package com.example.android.fragments;
 
--- app/src/main/java/com/example/android/fragments/MainActivity.java
+++ app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * The main activity when the app starts.
  */
 package com.example.android.fragments;
 
