138d7a9c4858f5d79f2ec9adf0966629257677ed
Johann Blake <polaris431@gmail.com>
2014-06-23 11:19:25 +0200

    Where was Johann

--- app/src/main/java/com/example/android/fragments/ArticleFragment.java
+++ app/src/main/java/com/example/android/fragments/ArticleFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * The fragment used for articles.
+ * The fragment used for articles. Johann was here.
  */
 package com.example.android.fragments;
 
