2f09078a8a08af5b08b20a92b9fcca5005c3b36f
Johann Blake <polaris431@gmail.com>
2014-06-23 12:34:54 +0200

    Added the Mammals class.

--- app/src/main/java/com/example/android/fragments/Mammals.java
+++ app/src/main/java/com/example/android/fragments/Mammals.java
@@ -1,7 +1,7 @@
 package com.example.android.fragments;
 
 /**
- * Created by Johann on 6/23/14. Whales, birds, dogs
+ * Created by Johann on 6/23/14. Whales, birds, dogs, cats, rats, mice
  */
 public class Mammals
 {
